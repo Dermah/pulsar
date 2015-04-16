219ce56eb3795fecc317d83ad362057acab32ad2
Dermah <inquiry@dermah.com>
2015-04-16 17:20:01 +1000

    Add index that serves a basic page

---
+++ index.js
@@ -0,0 +1,33 @@
+tty = require('tty');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/pages/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+var stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+stdin.on( 'data', function( key ){
+  // ctrl-c ( end of text )
+  if ( key === '\u0003' ) {
+    process.exit();
+  } else {
+    io.emit('pulsar', 'lol');
+  }
+});
