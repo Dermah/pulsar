41b2bdf41d7902a4947400fd52a6a750bea046f2
Dermah <inquiry@dermah.com>
2015-04-22 16:52:19 +1000

    Change to p5.js

--- pulsar.js
+++ pulsar.js
@@ -1,17 +1,9 @@
-var two = new Two({
-  type: Two.Types.webgl,
-  fullscreen: true,
-  autostart: true
-}).appendTo(document.body);
+var socket;
 
-var socket = io();
+function setup() {
+  socket = io.connect('http://localhost:3000');
+}
 
-var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
-two.bind('update', function() {
-  rect.rotation += 0.001;
-});
-
-socket.on('pulsar', function(msg) {
-  var newRect = two.makeRectangle(two.width * Math.random(), two.height * Math.random(), 50, 50);
-  console.log("Rec: " + msg);
-});
+function draw() {
+  // put drawing code here
+}
