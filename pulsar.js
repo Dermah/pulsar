7165b03fe20e8dd6d26ffc8afbd46d4b2fb1ff5b
Dermah <inquiry@dermah.com>
2015-04-22 17:40:18 +1000

    Resize p5.js canvas when window is resized

--- pulsar.js
+++ pulsar.js
@@ -2,8 +2,16 @@ var socket;
 
 function setup() {
   socket = io.connect('http://localhost:3000');
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  fill(0,0,255);
+  noStroke();
+  ellipse(12, 40, 80, 80);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
