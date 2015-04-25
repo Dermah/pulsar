b7399df926d56e4020b6e9210af088d7eca62df1
Dermah <inquiry@dermah.com>
2015-04-25 17:14:54 +1000

    Convert sketch to instance mode

--- pulsar.js
+++ pulsar.js
@@ -1,21 +1,26 @@
-var socket;
+var sketch = function ( p ) {
 
-function setup() {
-  socketSetup(socket);
-  createCanvas(windowWidth, windowHeight);
+  var socket;
+
+  p.setup = function() {
+    socketSetup(socket);
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  }
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(0,0,220);
+    p.noStroke();
+    p.ellipse(p.windowWidth/2, p.windowHeight/2, 80, 80);
+  }
+
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  }
+
+  function socketSetup (s) {
+    s = io.connect('http://localhost:3000');
+  }
 }
 
-function draw() {
-  background(0);
-  fill(0,0,220);
-  noStroke();
-  ellipse(windowWidth/2, windowHeight/2, 80, 80);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-function socketSetup (s) {
-  s = io.connect('http://localhost:3000');
-}
+var myp5 = new p5(sketch);
