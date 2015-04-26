9088000aa837de7bd5de56b7d8b70ee2a03f9238
Sputnik Antolovich <inquiry@dermah.com>
2015-04-27 02:40:37 +1000

    Enable different colours in the flash pulse

--- pulses/flash.js
+++ pulses/flash.js
@@ -1,17 +1,42 @@
 var Drawing = function (pulse) {
-	pulse.framesLeft = 600;
+	pulse.framesLeft = 15;
+    console.log(pulse);
+  if (!pulse.r) {
+    pulse.r = 255;
+  }
+  if (!pulse.g) {
+    pulse.g = 255;
+  }
+  if (!pulse.b) {
+    pulse.b = 255;
+  }
 
 	this.pulse = pulse;
-	console.log("Something!");
 };
 
 Drawing.prototype.draw = function (p) {
-  console.log('Drawing! ' + this.pulse);
+  p.rectMode(p.CORNER);
+
+
+
+  p.fill('rgba(' + 
+    this.pulse.r + ',' +
+    this.pulse.g + ',' +
+    this.pulse.b + ',' + 
+    ((this.pulse.framesLeft)/15) + ')');
+  
+  p.rect(0, 0, p.windowWidth, p.windowHeight);
 
   this.pulse.framesLeft--;
-  if (this.pulse.framesLeft < 0) {
-  	console.log("OMG");
-  }
+  
 };
+
+Drawing.prototype.done = function () {
+  if (this.pulse.framesLeft < 0) {
+  	return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = Drawing;
