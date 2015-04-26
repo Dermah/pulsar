7adf259618857831647c05cc27fcf26462fbe72e
Dermah <inquiry@dermah.com>
2015-04-26 18:07:18 +1000

    Whitespace fix

--- Receiver.js
+++ Receiver.js
@@ -1,24 +1,19 @@
 var events = require("events");
 var io = require('socket.io-client');
 
-
-
-var Receiver = function () {
-    
-};
-
+var Receiver = function () {};
 Receiver.prototype = new events.EventEmitter;
 
 Receiver.prototype.connect = function() {
     
-    var self = this;
-    var socket = io.connect('http://localhost:3000');
-    console.log('Connected to socket server');
-    
-    socket.on('pulse', function(data) {
-        console.log('Received pulse');
-        self.emit('received', data);
-    });
+  var self = this;
+  var socket = io.connect('http://localhost:3000');
+  console.log('Connected to socket server');
+  
+  socket.on('pulse', function(data) {
+    console.log('Receiver: Received pulse');
+    self.emit('received', data);
+  });
 }
 
 module.exports = Receiver;
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/pulsar.js', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
