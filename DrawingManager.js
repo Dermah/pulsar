de023d3df490721a9f254b7537356e2b57289b8c
Sputnik Antolovich <inquiry@dermah.com>
2015-04-27 02:40:10 +1000

    Add add garbage collection and fix drawing order in the manager

--- DrawingManager.js
+++ DrawingManager.js
@@ -5,14 +5,19 @@ var DrawingManager = function (p5Sketch) {
   p = p5Sketch;
 };
 
-DrawingManager.prototype.add = function (pulse) {
-  console.log('Added ' + pulse);
-  drawings.push(pulse);
+DrawingManager.prototype.add = function (drawing) {
+  console.log('Added ' + drawing);
+  drawings.unshift(drawing);
 }
 
 DrawingManager.prototype.drawAll = function () {
   for (var i = drawings.length - 1; i >= 0; i--) {
     drawings[i].draw(p);
+
+    if (drawings[i].done()) {
+      drawings.splice(i, 1);
+      i--;
+    }
   };
 }
 
