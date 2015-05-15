6504226808138e11cf7e5d601c293096d534b229
Sputnik Antolovich <inquiry@dermah.com>
2015-05-15 23:42:50 +1000

    Make the processor reject pulses with targets that don't match the client config

--- src/Processor.js
+++ src/Processor.js
@@ -5,6 +5,19 @@ require('./pulses/pulsar-splash.js');
 var Processor = function () {};
 
 Processor.prototype.createDrawing = function (pulse, config) {
+  if (pulse.target) {
+    for (var key in pulse.target) {
+      if (config.hasOwnProperty(key) && config[key] === pulse.target[key]) {
+        // so far so good
+      } else {
+        // a target property was not present on this client or
+        // a target property did not match on this client
+        // exiting early
+        return null;
+      }
+    }
+  }
+
   var Drawing = require('./pulses/' + pulse.name + '.js');
   var drawing = new Drawing(pulse, config);
 
