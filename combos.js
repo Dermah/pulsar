38a04e2ec75aac844eaed0b7cf7a9c9598ec06ee
Dermah <inquiry@dermah.com>
2015-05-30 13:57:47 +1000

    Add target option to combos

--- combos.js
+++ combos.js
@@ -3,13 +3,18 @@ var Combos = function (theIO) {
   io = theIO;
 };
 
-Combos.prototype.randomFlash = function() {
-  io.emit('pulse',  { 
+Combos.prototype.randomFlash = function(target) {
+  var pulse = { 
     name: 'flash',
     r: Math.floor(Math.random() * 255),
     g: Math.floor(Math.random() * 255),
     b: Math.floor(Math.random() * 255)
-  });
+  }
+
+  if (target) {
+    pulse.target = target;
+  }
+  io.emit('pulse',  pulse);
 }
 
 Combos.prototype.flashRow = function(row) {
@@ -42,30 +47,34 @@ Combos.prototype.flashUp = function(rate) {
   setInterval(march, 4*rate, rate);
 }
 
-Combos.prototype.atmospheric = function (length) {
-  var emitall = function() {
-    io.emit('pulse', {
+Combos.prototype.atmospheric = function (length, target) {
+  var pusle = {
       name: 'fade',
       totalFrames: 12,
       r: Math.ceil(Math.random()*25),
       g: Math.ceil(Math.random()*25),
       b: Math.ceil(Math.random()*120),
       probability: 0.1,
-    });
+    }
+  if (target) {
+    pulse.target = target;
+  }
+  var emitall = function() {
+    io.emit('pulse', pulse);
   };
   emitall();
   var atmos = setInterval(emitall, 40)
   setTimeout(clearInterval, length, atmos);
 }
 
-Combos.prototype.glock3 = function (note) {
+Combos.prototype.glock3 = function (note, target) {
   // note should be number 1 to 3
 
   var offset = (note - 2) * 0.333333333;
 
-  io.emit('pulse', {
+  var pulse = {
     name: 'bars',
-    totalFrames: 8,
+    totalFrames: 16,
     r: (255),
     g: (200 - Math.floor(Math.random()*200)),
     b: (200 - Math.floor(Math.random()*200)),
@@ -73,15 +82,26 @@ Combos.prototype.glock3 = function (note) {
     w: 1,
     y: 0.5 + offset,
     h: 0.33333,
-  });
+  }
+
+  if (target) {
+    pulse.target = target;
+  }
+
+  io.emit('pulse', pulse);
 }
 
-Combos.prototype.slideUpDouble = function () {
+Combos.prototype.slideUpDouble = function (frames, target) {
+  var pulse = {
+    name: 'slider',
+    totalFrames: 1
+  }
+  if (target) {
+    pulse.target = target;
+  }
   var emitter = function(frames) {
-    io.emit('pulse', {
-      name: 'slider',
-      totalFrames: frames
-    });
+    pulse.totalFrames = frames;
+    io.emit('pulse', pulse);
   }
   
   emitter(22);
@@ -89,16 +109,22 @@ Combos.prototype.slideUpDouble = function () {
   setTimeout(emitter, 750, 25);
 }
 
-Combos.prototype.calmBeforeStorm = function () {
-  io.emit('pulse', {
+Combos.prototype.calmBeforeStorm = function (target) {
+  var pulse = {
     name: 'strobe',
     totalFrames: 11,
     framesOn: 10,
-    probability: 0.5,
+    probability: 0.6,
     r: 255,
     g: 100,
     b: 100
-  });
+  }
+
+  if (target) {
+    pulse.target = target;
+  }
+
+  io.emit('pulse', pulse);
 }
 
 module.exports = Combos;
