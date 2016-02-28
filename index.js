19024eb1ca8e699e798f45543a3346d4337f563f
Sputnik <inquiry@dermah.com>
2016-02-29 00:12:55 +1100

    Delete pulsar-transmitter code that has been extracted out

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Server side definition of what the PULSAR 
+// Server side definition of what the PULSAR
 // grid looks like (how many rows and columns)
 var config = require('./config.json');
-var transmitter = require('./lib/transmitter')(config);
+var transmitter = require('@dermah/pulsar-transmitter')(config);
