2c0b8b27c554fc167a584a3a4b9f190e87435490
Sputnik Antolovich <inquiry@dermah.com>
2016-06-23 21:55:11 +1000

    Use new emitter detector versions of input-keybaord and transmitter

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Server side definition of what the PULSAR
 // grid looks like (how many rows and columns)
-var config = require('./config.json');
-var transmitter = require('@dermah/pulsar-transmitter')(config);
+let config = require('./config.json');
+
+let Input = require('@dermah/pulsar-input-keyboard');
+let Detector = require('@dermah/pulsar-transmitter');
+
+let input = new Input(config);
+let detector = new Detector(config);
+
+input.on('pulse', pulse => {
+  detector.detect('pulse', pulse);
+});
+input.on('pulsar control', pulse => {
+  detector.detect('pulsar control', pulse);
+});
+input.on('pulse update', pulse => {
+  detector.detect('pulse update', pulse);
+});
